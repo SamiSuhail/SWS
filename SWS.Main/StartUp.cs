7da325b25707c8b5b9d5e3b024fd12e1aca29892
SamiSuhail <sami.suhail.off@gmail.com>
2021-06-17 12:15:29 +0300

    Initial Commit (Complete)

---
+++ SWS.Main/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using SWS.Server;
+
+namespace SWS
+{
+    public class StartUp
+    {
+        static async Task Main()
+        {
+            var server = new HttpServer();
+
+            await server.Start();
+        }
+    }
+}
---
+++ SWS.Server/Http/HttpServer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWS.Server
+{
+    public class HttpServer
+    {
+        private const string ipAddress = "127.0.0.1";
+        private const int defaultPort = 9090;
+        private TcpListener serverListener;
+        public HttpServer(string ipAddress, int port)
+        {
+            var temp = IPAddress.Parse(ipAddress);
+            serverListener = new TcpListener(temp, port);
+            Console.WriteLine($"Server started on port {port}, waiting for client connection...");
+            serverListener.Start();
+        }
+
+        public HttpServer()
+            :this(ipAddress, defaultPort)
+        {
+        }
+
+        public async Task Start()
+        {
+            while (true)
+            {
+                var connection = await serverListener.AcceptTcpClientAsync();
+                Console.WriteLine();
+                Console.WriteLine("NEW CONNECTION STARTED");
+                Console.WriteLine();
+                var networkStream = connection.GetStream();
+
+                var request = await GetRequest(networkStream);
+                Console.WriteLine();
+                Console.WriteLine("RECEIVED REQUEST:");
+                Console.WriteLine(request);
+
+
+                Console.WriteLine();
+                Console.WriteLine("SENDING RESPONSE:");
+                var content = "Hello World";
+                var response = SetResponse(content);
+                Console.WriteLine(Encoding.UTF8.GetString(response));
+
+                await networkStream.WriteAsync(response);
+
+                connection.Close();
+            }
+        }
+
+        public async Task<string> GetRequest(NetworkStream networkStream)
+        {
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
+            var sb1 = new StringBuilder();
+
+            while (networkStream.DataAvailable)
+            {
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
+                sb1.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+
+            return sb1.ToString();
+        }
+
+        public byte[] SetResponse(string content)
+        {
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+            var sb2 = new StringBuilder();
+            sb2.AppendLine("HTTP/1.1 200 OK");
+            sb2.AppendLine("Server: SoftUni Web Server");
+            sb2.AppendLine($"Date: {DateTime.UtcNow.ToString("r")}");
+            sb2.AppendLine($"Content-Length: {contentLength}");
+            sb2.AppendLine("Content-Type: text/plain; charset=UTF-8");
+            sb2.AppendLine();
+            sb2.AppendLine(content);
+
+            var response = Encoding.UTF8.GetBytes(sb2.ToString());
+            return response;
+        }
+    }
+}
